0a2f4e9412ea21005e2c921c36985d25ef963674
Sanyam96 <sanyam.bvcoe96@gmail.com>
2018-09-12 15:27:23 +0530

    working code

---
+++ src/main/java/com/nagarro/controllers/LoginController.java
@@ -0,0 +1,36 @@
+package com.nagarro.controllers;
+
+import com.nagarro.models.LoginBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Sanyam Goel created on 12/9/18
+ */
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String init(Model model) {
+        model.addAttribute("msg", "Please Enter Your Login Details");
+        return "login";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submit(Model model, @ModelAttribute("loginBean") LoginBean loginBean) {
+        if (loginBean != null && loginBean.getUserName() != null & loginBean.getPassword() != null) {
+            if (loginBean.getUserName().equals("user") && loginBean.getPassword().equals("user")) {
+                model.addAttribute("msg", loginBean.getUserName());
+                return "flights";
+            } else {
+                model.addAttribute("error", "Invalid Details");
+                return "login";
+            }
+        } else {
+            model.addAttribute("error", "Please enter Details");
+            return "login";
+        }
+    }
+}
