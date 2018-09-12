21dbc6bbd7e712e9bebeb406f512308027358c84
Sanyam96 <sanyam.bvcoe96@gmail.com>
2018-09-12 12:47:44 +0530

    sorting

---
+++ src/main/java/com/nagarro/enums/FlightOutputType.java
@@ -0,0 +1,9 @@
+package com.nagarro.enums;
+
+/**
+ * @author Sanyam Goel created on 12/9/18
+ */
+public enum FlightOutputType {
+    BY_FARE,
+    BY_FARE_DURATION
+}
--- src/main/java/com/nagarro/service/FlightServiceImpl.java
+++ src/main/java/com/nagarro/service/FlightServiceImpl.java
@@ -1,6 +1,7 @@
 package com.nagarro.service;
 
 import com.nagarro.dao.FlightDao;
+import com.nagarro.enums.FlightOutputType;
 import com.nagarro.interfaces.FlightService;
 import com.nagarro.models.Flight;
 import com.nagarro.models.FlightSearchDetails;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.text.ParseException;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -34,6 +36,21 @@ public class FlightServiceImpl implements FlightService {
 
     @Transactional
     public List<Flight> findMatchedFlight(FlightSearchDetails flightSearchDetails) throws ParseException {
-        return flightDao.findMatchedFlight(flightSearchDetails);
+        List<Flight> flights = flightDao.findMatchedFlight(flightSearchDetails);
+
+        FlightOutputType flightOutputType = null;
+        String preference = flightSearchDetails.getSortPreference().toUpperCase();
+        flightOutputType = preference.contains("FARE") ? FlightOutputType.BY_FARE : FlightOutputType.BY_FARE_DURATION;
+        switch (flightOutputType) {
+            case BY_FARE:
+                flights.sort(Comparator.comparing(Flight::getFlightFare));
+                break;
+            case BY_FARE_DURATION:
+                flights.sort(Comparator.comparing(Flight::getFlightFare).thenComparing(Flight::getFlightDuration));
+                break;
+            default:
+                break;
+        }
+        return flights;
     }
 }
